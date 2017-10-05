a9c891c8c3397c7c4ffd43220d4ed9251f6467e4
Nilo Ballener <nballener@gmail.com>
2017-10-05 17:06:27 +1100

    init calculations

--- js/energycalculator.js
+++ js/energycalculator.js
@@ -3,7 +3,19 @@ window.addEventListener('load', function () {
   var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      wattage: 0.0,
+      price: 0.0,
+      usageHours: 0,
+      cost: 0.0,
+      dailyCost: 0.0
+    },
+    methods: {
+      calculateCost: function () {
+        let wattageHour = this.wattage * this.usageHours
+        let kiloWattageHour = wattageHour / 1000.00
+
+        this.dailyCost = kiloWattageHour * this.price
+      }
     }
   })
 
